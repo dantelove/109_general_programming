377c3c9efbf3dc995d8fb596389a0726bd69ccce
Dante Love <outlineandconquer@gmail.com>
2016-09-06 15:50:01 -0700

    Beginning Ruby - Video 4 - Exercise 1

---
+++ beginning_ruby/part_4/beginning_ruby_4_1.rb
@@ -0,0 +1,23 @@
+# beginning_ruby_4_1.rb
+
+# take a string as an argument and return the string in reverse order without
+# using the reverse method
+
+#this is my attempt after hearing the exercise, but before watching Derek
+#attempt to solve it.
+
+def manual_reverse(str)
+    str = str.split
+    
+    new_string = []
+    
+    str.length.times do
+        new_string << str.last
+        str.pop
+    end
+    
+    p new_string
+end
+
+manual_reverse("This is a longer string.")
+manual_reverse("This is an even longer string.")
