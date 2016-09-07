c6510bb09fc74d9ac8e7a8058dd164a085badb06
Dante Love <outlineandconquer@gmail.com>
2016-09-07 14:21:17 -0700

    Beginning Ruby - Part 4 - Exercise 2

---
+++ beginning_ruby/part_4/beginning_ruby_4_2.rb
@@ -0,0 +1,30 @@
+# beginning_ruby_4_2.rb
+
+# Write a method that takes two arguments: the first is the
+# starting number and the second is the ending number. Print
+# out all numbers between the two numbers, except if a number
+# is divisible 3 print "Fizz", is a number is divisible by 5,
+# print "Buzz", and finally if a number is divisible by 3 and 5,
+# print "FizzBuzz".
+
+#Exercise 2
+
+# this is my attempt before watching the video walkthrough.
+
+def fizzbuzz(num1, num2)
+    numbers = num1..num2
+    numbers.each do |x|
+        case
+        when x % 3 == 0 && x % 5 == 0
+            puts "FizzBuzz"
+        when x % 3 == 0
+            puts "Fizz"
+        when x % 5 == 0
+            puts "Buzz"
+        else
+            puts x
+        end
+    end
+end
+
+fizzbuzz(1, 20)
