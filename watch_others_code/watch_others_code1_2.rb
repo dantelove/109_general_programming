661b25be31ec69468d50589a172e0ff9180b32af
Dante Love <outlineandconquer@gmail.com>
2016-09-12 15:41:52 -0700

    Watch Others Code - Part 1 - Exercise 2

---
+++ watch_others_code/watch_others_code1_2.rb
@@ -0,0 +1,39 @@
+# watch_others_code1_2.rb
+
+# Write a method that will determine whether an integer is a prime.
+# Don't use the prime classe.
+
+# is_prime?(3)
+# is_prime?(4)
+
+def is_prime?(num)
+  range = (2..(num - 1))
+  range.select do |x| #refactored to remove range variable
+    return false if num % x == 0
+  end
+  return true #moved the this return to solve is_prime?(2) while watching video
+end
+
+# p is_prime?(3)
+# p is_prime?(4)
+# p is_prime?(2)
+
+# Write a method that will take an array of numbers, and only return those that
+# are prime
+
+# select_primes ([1, 2, 3, 4, 5] => [2, 3])
+# select_primes ([4,6,8,10]) => []
+
+#This is my attempt before watching the walkthrough
+
+#Not sure what to do about 1.
+
+array1 = [1, 2, 3, 4]
+array2 = [4, 6 , 8, 10]
+
+def select_primes(array)
+  array.select { |num| is_prime?(num) }
+end
+
+p select_primes(array1)
+p select_primes(array2)
