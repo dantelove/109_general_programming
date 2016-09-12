c71415a4a503b4d23c2e621ba1515c577d181817
Dante Love <outlineandconquer@gmail.com>
2016-09-12 15:04:59 -0700

    Watch Others Code - Part 1 - Exercise 1

---
+++ watch_others_code/watch_others_code1_1.rb
@@ -0,0 +1,20 @@
+# watch_others_code1_1.rb
+
+# Write a method that will determine whether an integer is a prime.
+# Don't use the prime classe.
+
+# is_prime?(3)
+# is_prime?(4)
+
+#This is my attempt before watching the walkthrough
+
+def is_prime?(num)
+  range = (2..(num - 1)).to_a
+  range.select do |x|
+    return false if num % x == 0
+    return true
+  end
+end
+
+p is_prime?(3)
+p is_prime?(4)
