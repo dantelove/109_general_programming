6536b922681edea542f5e13cf86662ad14736880
Dante Love <outlineandconquer@gmail.com>
2016-09-23 14:20:58 -0700

    Watch Others Code - Part 4 - Exercise 3

---
+++ watch_others_code/watch_others_code4_3.rb
@@ -0,0 +1,30 @@
+# watch_others_code4_3.rb
+
+# Write a method that will generate random english math problems.
+# The method should take a length, then return a math phrase with
+# that many operations.
+
+# Example:
+
+# math_phrase(1) # => "five minus two"
+# math_phrase(2) # => "two plus three times eight"
+# math_phrase(3) # => "one divided by three plus five times zero"
+
+# Given
+
+NUMBERS = %w(zero one two three four five six seven eight nine ten)
+OPERATORS = %w(plus minus times divided\ by)
+
+def math_phrase(num)
+  phrase_array = ["#{NUMBERS.sample}"]
+
+  num.times do 
+    phrase_array << "#{OPERATORS.sample} #{NUMBERS.sample}"
+  end
+  
+  phrase_array.join(" ").to_s
+end
+
+p math_phrase(1) # => "five minus two"
+p math_phrase(2) # => "two plus three times eight"
+p math_phrase(3) # => "one divided by three plus five times zero"
