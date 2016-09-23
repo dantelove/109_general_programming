da141530311af2d53c3a563d33442d4edde796c6
Dante Love <outlineandconquer@gmail.com>
2016-09-23 14:44:50 -0700

    Watch Others Code - Part 4 - Exercise 3B

--- watch_others_code/watch_others_code4_3b.rb
+++ watch_others_code/watch_others_code4_3b.rb
@@ -1,4 +1,4 @@
-# watch_others_code4_3.rb
+# watch_others_code4_3b.rb
 
 # Write a method that will generate random english math problems.
 # The method should take a length, then return a math phrase with
@@ -17,16 +17,36 @@
 NUMBERS = %w(zero one two three four five six seven eight nine ten)
 OPERATORS = %w(plus minus times divided\ by)
 
-def math_phrase(num)
+# def math_phrase(num)
+#   phrase_array = ["#{NUMBERS.sample}"]
+
+#   num.times do 
+#     phrase_array << "#{OPERATORS.sample} #{NUMBERS.sample}"
+#   end
+
+#   phrase_array.join(" ").to_s
+# end
+
+# p math_phrase(1) # => "five minus two"
+# p math_phrase(2) # => "two plus three times eight"
+# p math_phrase(3) # => "one divided by three plus five times zero"
+
+# Write a program that will generate random english math problems of
+# varying length.
+
+def math_phrase
   phrase_array = ["#{NUMBERS.sample}"]
 
-  num.times do 
+  rand(1..10).times do 
     phrase_array << "#{OPERATORS.sample} #{NUMBERS.sample}"
   end
 
   phrase_array.join(" ").to_s
 end
 
-p math_phrase(1) # => "five minus two"
-p math_phrase(2) # => "two plus three times eight"
-p math_phrase(3) # => "one divided by three plus five times zero"
+p math_phrase
+p math_phrase
+p math_phrase
+p math_phrase
+p math_phrase
+p math_phrase
