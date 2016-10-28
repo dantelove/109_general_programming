d0e37515ac78571b151f79236500a923b74e1f34
Dante Love <outlineandconquer@gmail.com>
2016-10-28 09:11:14 -0700

    Small Problems - Easy 9 - Exercises 1 - 10

---
+++ review/small_problems/9_easy9/easy9_7.rb
@@ -0,0 +1,12 @@
+# easy9_7.rb
+
+# Write a method that takes a first name, a space, and a last name 
+# passed as a single String argument, and returns a string that 
+# contains the last name, a comma, a space, and the first name.
+
+def swap_name(string)
+  first, last = string.split
+  "#{last}, #{first}"
+end
+
+p swap_name('Joe Roberts') == 'Roberts, Joe'
